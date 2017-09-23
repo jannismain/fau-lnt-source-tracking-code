0f42f5d70b0a82604fad9c15e3252d1367b46f6c
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-09-23 16:35:53 +0100

    improved eval script output

--- matlab/mainczjs/evaluation/random_sources_eval.m
+++ matlab/mainczjs/evaluation/random_sources_eval.m
@@ -1,6 +1,6 @@
-function random_sources_eval = random_sources_eval(n_sources, trials, min_distance)
-tic;
+function loc_err = random_sources_eval(n_sources, trials, min_distance)
 
+%% function argument handling
 if nargin < 1
     n_sources = 2;
 end
@@ -11,23 +11,34 @@ if nargin < 3
     min_distance = 0.5;
 end
 
+%% initialisation
+cprintf('-comment', '                E V A L U A T I O N                \n');
 loc_err = zeros(trials, n_sources);
 
+%% trials
 for trial=1:trials
-    config_update(n_sources, true, min_distance);
+    cprintf('*blue', '   [Trial %d/%d]: %d sources, %1.2f minimal distance\n', trial, trials, n_sources, min_distance/10);
+    tic;
+    log_cfg_update = evalc('config_update(n_sources, true, min_distance);');
     load('config.mat');
-    [x, fig] = simulate(ROOM, R, S);
-    [X, phi] = stft(x);
+    for s=1:n_sources
+        fprintf("%s Source Location #%d = [x=%0.2f, y=%0.2f]\n", FORMAT_PREFIX, s, S(s,1:2));
+    end
+    [log_simulate, x, fig] = evalc('simulate(ROOM, R, S);');
+%     [x, fig] = simulate(ROOM, R, S);
+    [log_stft, X, phi] = evalc('stft(x);');
     cfg = set_params_evaluate_Gauss();
     subplot_tight(2,2,[2 4], PLOT_BORDER);
-    [psi, est_error1, est_error2] = bren_estimate_location(cfg, phi);
+    [log_loc_est, psi, est_error1, est_error2] = evalc('bren_estimate_location(cfg, phi);');
     if trials>1
         loc_err(trial, :) = [est_error1, est_error2];
     else
         loc_err = [est_error1, est_error2];
     end
+    fprintf("%s Localisation Estimation Error = [%0.2f, %0.2f]\n", FORMAT_PREFIX, est_error1, est_error2)
 end
 
-random_sources_eval = mean(loc_err, 1);
+%% results
+cprintf('*err', '   [   RESULT]: Mean Localisation Error = %0.2f (Total Runtime = %s)\n', mean(mean(loc_err)), num2str(toc)');
 
 end
