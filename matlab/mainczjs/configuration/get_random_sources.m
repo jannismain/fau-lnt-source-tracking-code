b828ba7aeae61f99f1dc5c151acc29a792df2d4c
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-09-27 10:55:11 +0100

    clarified function description

--- matlab/mainczjs/configuration/get_random_sources.m
+++ matlab/mainczjs/configuration/get_random_sources.m
@@ -3,8 +3,8 @@ function S = get_random_sources(n_sources, distance_wall, distance_sources, ROOM
 %
 % n_sources = number of sources (int)
 % distance_wall = distance from wall [decimeter] (default: 12)
-% distance_sources = required distance of source from each other
-% [decimeter] (default: 5)
+% distance_sources = required distance of source from each other (in both x and y
+% direction) [decimeter] (default: 5)
 
 if nargin < 2, distance_wall = 12; end
 if nargin < 3, distance_sources = 5; end
@@ -28,6 +28,6 @@ x_done = false; y_done = false; i = 0;
 
     S      = [x_rand y_rand ones(n_sources, 1)];
     distance = min(abs(diff(x_rand)))+min(abs(diff(y_rand)));
-    fprintf("      ...done! (found %i sufficiently spaced sources (min_distance = %0.2f) after %i attempts)\n", n_sources, distance, i);
+    fprintf("      -> done! (found %i sufficiently spaced sources (distance = %0.2f) after %i attempts)\n", n_sources, distance, i);
 
 end
