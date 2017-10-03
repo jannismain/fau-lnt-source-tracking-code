dedac0e39533c04cce70fd9e5465760af37aee08
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-10-03 15:07:23 +0100

    loop for "different number of source" trials

--- matlab/mainczjs/evaluation/main.m
+++ matlab/mainczjs/evaluation/main.m
@@ -1,12 +1,9 @@
-%% Evaluation 1: 2 sources, 0.50m distance from each other, 1.20m distance from wall
-trials = 2;
-sources = 2;
+%% Evaluation 1: 2/3/4 sources, 0.50m distance from each other, 1.20m distance from wall
+trials = 20;
 min_distance = 5;
 distance_wall = 12;
-results = random_sources_eval(sources,trials,min_distance,distance_wall);
+for sources = 3:3
+    results = random_sources_eval(sources,trials,min_distance,distance_wall);
+end
 
-% %% Evaluation 1: 3 sources, 0.50m minimal distance
-% trials = 100;
-% sources = 3;
-% min_distance = 5;
-% loc_err = random_sources_eval(sources,trials,min_distance);
+%% Evaluation 2: 
