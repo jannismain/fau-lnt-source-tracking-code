81b1e39d25fcb1023b51cab542991c0541fd0161
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-10-12 12:38:44 +0100

    update

--- matlab/mainczjs/evaluation/main.m
+++ matlab/mainczjs/evaluation/main.m
@@ -1,9 +1,15 @@
-%% Evaluation 1: 2/3/4 sources, 0.50m distance from each other, 1.20m distance from wall
-trials = 20;
+%% Evaluation 1: 2-7 sources, 0.50m distance from each other, 1.20m distance from wall
+trials = 10;
 min_distance = 5;
 distance_wall = 12;
-for sources = 3:3
+for sources = 2:7
     results = random_sources_eval(sources,trials,min_distance,distance_wall);
 end
 
-%% Evaluation 2: 
+%% Evaluation 2: 2-7 sources, 0.50m distance from each other, 1.20m distance from wall
+% trials = 50;
+% min_distance = 5;
+% distance_wall = 12;
+% for sources = 2:7
+%     results = random_sources_eval(sources,trials,min_distance,distance_wall);
+% end
