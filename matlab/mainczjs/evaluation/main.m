d39c0d5d946587e8c9c7c28814bb33e95b75f32e
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-10-18 01:30:43 +0100

    defined evaluation runs

--- matlab/mainczjs/evaluation/main.m
+++ matlab/mainczjs/evaluation/main.m
@@ -1,23 +1,23 @@
-%% Evaluation 1: 2-7 sources, 0.50m distance from each other, 1.20m distance from wall
-% trials = 50;
-% min_distance = 5;
-% distance_wall = 12;
-% for sources = 2:7
-%     results = random_sources_eval(sources,trials,min_distance,distance_wall);
-% end
-
-%% Evaluation 2: 2-7 sources, 0.50m distance from each other, 1.20m distance from wall
-% trials = 50;
-% min_distance = 5;
-% distance_wall = 12;
-% for sources = 2:7
-%     results = random_sources_eval(sources,trials,min_distance,distance_wall);
-% end
-
-%% Evaluation 3: randomized source samples
-trials = 20;
+%% Evaluation 1: BASE (randomised source samples, md=5, wd=12, em=5)
+trials = 45;
 min_distance = 5;
 distance_wall = 12;
-for sources = 2:2
+for sources = 2:5
     results = random_sources_eval(sources,trials,min_distance,distance_wall,true,0.0,0,5);
 end
+
+%% Evaluation 2: T60=0.3 (randomised source samples, md=5, wd=12)
+% trials = 20;
+% min_distance = 5;
+% distance_wall = 12;
+% for sources = 2:5
+%     results = random_sources_eval(sources,trials,min_distance,distance_wall,true,0.3,0,5);
+% end
+
+%% Evaluation 3: T60=0.6 (randomised source samples, md=5, wd=12)
+% trials = 20;
+% min_distance = 5;
+% distance_wall = 12;
+% for sources = 2:5
+%     results = random_sources_eval(sources,trials,min_distance,distance_wall,true,0.6,0,5);
+% end
