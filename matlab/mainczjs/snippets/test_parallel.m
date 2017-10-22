6fcd16cfbf829bab087374b16467278a2037faf1
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-10-22 22:21:33 +0200

    test GPU processing

--- matlab/mainczjs/snippets/test_parallel.m
+++ matlab/mainczjs/snippets/test_parallel.m
@@ -1,17 +1,25 @@
-tic
-n = 100;
-A = 500;
-a = zeros(n);
-for i = 1:n
-    a(i) = max(abs(eig(rand(A))));
-end
-toc
+% tic
+% n = 100;
+% A = 500;
+% a = zeros(n);
+% for i = 1:n
+%     a(i) = max(abs(eig(rand(A))));
+% end
+% toc
+% 
+% tic
+% n = 100;
+% A = 500;
+% a = zeros(n);
+% parfor i = 1:n
+%     a(i) = max(abs(eig(rand(A))));
+% end
+% toc
 
-tic
-n = 100;
-A = 500;
-a = zeros(n);
-parfor i = 1:n
-    a(i) = max(abs(eig(rand(A))));
+
+% Test GPU Devices
+for ii = 1:gpuDeviceCount
+    g = gpuDevice(ii);
+    fprintf(1,'Device %i has ComputeCapability %s \n', ...
+            g.Index,g.ComputeCapability)
 end
-toc
