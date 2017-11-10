8994b16a773f3a26d1b166f00b09f2a8b731cc4c
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-11-10 02:13:08 +0100

    blub

--- matlab/mainczjs/plot_results.m
+++ matlab/mainczjs/plot_results.m
@@ -15,7 +15,7 @@ function [ fig ] = plot_results( psi, loc_est, room)
                   'Color','white',...
                   'Position', [fig_xpos fig_ypos fig_size(1) fig_size(2)]);%,...
                   %'Visible','off');  TODO: Comment this back in for trial runs!
-    
+
     if iterations>1  % plot entire em-step history
         for i=1:iterations
             subplot_tight(2,iterations,i)
@@ -24,13 +24,13 @@ function [ fig ] = plot_results( psi, loc_est, room)
             set(gca, 'box', 'off')
             hold on
 
-            plot(room.R(:, 1), room.R(:, 2),'x','MarkerSize', 12, 'Linewidth',2,'Color','g');    
+            plot(room.R(:, 1), room.R(:, 2),'x','MarkerSize', 12, 'Linewidth',2,'Color','g');
             plot(room.S(:, 1), room.S(:, 2),'x','MarkerSize', 16, 'Linewidth',2,'Color','w');
             plot(loc_est(:, 1), loc_est(:, 2),'x','MarkerSize', 16, 'Linewidth',2,'Color','r');
 
             axis([0,room.dimensions(1),0,room.dimensions(2)]);
             subplot_tight(2,iterations,i+iterations)
-            fprintf("grid_x = %dx%d, grid_y = %dx%d, psi = %dx%d", size(room.grid_x, 1), size(room.grid_x, 2), size(room.grid_y, 1), size(room.grid_y, 2), size(psi, 2), size(psi, 3))
+            fprintf('grid_x = %dx%d, grid_y = %dx%d, psi = %dx%d', size(room.grid_x, 1), size(room.grid_x, 2), size(room.grid_y, 1), size(room.grid_y, 2), size(psi, 2), size(psi, 3))
             surf(room.grid_x,room.grid_y,squeeze(psi(i,:,:)))
             view([-10 35]);
             shading interp
@@ -42,11 +42,11 @@ function [ fig ] = plot_results( psi, loc_est, room)
         set(gca, 'box', 'off')
         hold on
 
-        plot(room.R(:, 1), room.R(:, 2),'x','MarkerSize', 12, 'Linewidth',2,'Color','g');    
+        plot(room.R(:, 1), room.R(:, 2),'x','MarkerSize', 12, 'Linewidth',2,'Color','g');
         plot(room.S(:, 1), room.S(:, 2),'x','MarkerSize', 16, 'Linewidth',2,'Color','w');
         plot(loc_est(:, 1), loc_est(:, 2),'x','MarkerSize', 16, 'Linewidth',2,'Color','r');
         axis([0,room.dimensions(1),0,room.dimensions(2)]);
-        
+
         subplot_tight(1,2,2);
         surf(room.grid_x,room.grid_y,psi)
         view([-65 25]);
