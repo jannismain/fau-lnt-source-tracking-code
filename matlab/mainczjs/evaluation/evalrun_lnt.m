acabb5ce0785fe46c6c689884bd22a75de7635d2
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-11-12 17:29:53 +0100

    new evalrun

--- matlab/mainczjs/evaluation/evalrun_lnt.m
+++ matlab/mainczjs/evaluation/evalrun_lnt.m
@@ -5,7 +5,6 @@ rand('state',sum(100*clock));
 
 % EVALUATION ROUTINE:
 description='var-fixed';  % test fixed variance with different values
-trials=50;
 md = 5;
 wd = 12;
 rand_samples = true;
@@ -15,10 +14,11 @@ em_iterations=10;
 em_conv_threshold=-1;
 guess_randomly=false;
 reflect_order=3;
-var_init = [5 3];
+trials=[50 50 100 50];
+var_init = [0.1 0.5 1 2];
 var_fixed = true;
 for i=1:length(var_init)
     for sources = 2:7
-        random_sources_eval(description,sources,trials,md,wd,rand_samples,T60,SNR,em_iterations, em_conv_threshold, guess_randomly,reflect_order,var_init(i),var_fixed, '/HOMES/mainczyk/thesis/src/');
+        random_sources_eval(description,sources,trials(i),md,wd,rand_samples,T60,SNR,em_iterations, em_conv_threshold, guess_randomly,reflect_order,var_init(i),var_fixed, '/HOMES/mainczyk/thesis/src/');
     end
 end
