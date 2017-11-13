5f4bba8cb2de3ca04f1e5d1ff6ebbeba9c6b0ed2
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-11-13 02:33:30 +0100

    blub

--- matlab/mainczjs/evaluation/evalrun_lnt.m
+++ matlab/mainczjs/evaluation/evalrun_lnt.m
@@ -4,19 +4,19 @@ addpath_recurse  % ensure all folders are added to path
 rand('state',sum(100*clock));
 
 % EVALUATION ROUTINE:
-description='var-fixed';  % test fixed variance with different values
+description='noise';  % test fixed variance with different values
 md = 5;
 wd = 12;
 rand_samples = true;
 T60=0.3;
 SNR=0;
-em_iterations=10;
+em_iterations=5;
 em_conv_threshold=-1;
 guess_randomly=false;
 reflect_order=3;
-trials=[50];
-var_init = [1];
-var_fixed = [true];
+trials=[100];
+var_init = [0.1];
+var_fixed = [false];
 for i=1:length(var_init)
     for sources = 2:7
         random_sources_eval(description,sources,trials(i),md,wd,rand_samples,T60,SNR,em_iterations, em_conv_threshold, guess_randomly,reflect_order,var_init(i),var_fixed(i), '/HOMES/mainczyk/thesis/src/');
