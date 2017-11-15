c17e05be6f48dd763a44cc1ba962373ccaa4cbdd
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-11-16 00:37:51 +0100

    new evaluations

--- matlab/mainczjs/evaluation/evalrun_lnt.m
+++ matlab/mainczjs/evaluation/evalrun_lnt.m
@@ -4,21 +4,19 @@ addpath_recurse  % ensure all folders are added to path
 rand('state',sum(100*clock));
 
 % EVALUATION ROUTINE:
-description='em-iterations';  % test fixed variance with different values
+description='worst-case';
 md = 5;
-wd = [13,15];
+wd = 12;
 rand_samples = true;
-T60=0.3;
-SNR=0;
+T60=0.9;
+SNR=5;
 em_iterations=5;
 em_conv_threshold=-1;
 guess_randomly=false;
-reflect_order=3;
-trials=[25 25];
+reflect_order=-1;
+trials=10;
 var_init = 0.1;
 var_fixed = false;
-for i=1:length(wd)
-    for sources = 2:7
-        random_sources_eval(description,sources,trials(i),md,wd(i),rand_samples,T60,SNR,em_iterations, em_conv_threshold, guess_randomly,reflect_order,var_init,var_fixed, '/HOMES/mainczyk/thesis/src/');
-    end
+for sources = 2:7
+    random_sources_eval(description,sources,trials(i),md,wd(i),rand_samples,T60,SNR,em_iterations, em_conv_threshold, guess_randomly,reflect_order,var_init,var_fixed, '/HOMES/mainczyk/thesis/src/');
 end
