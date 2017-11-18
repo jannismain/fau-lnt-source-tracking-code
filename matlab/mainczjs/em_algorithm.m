bbc46068956c7ec5f0cc92d64d9732ca43f35902
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-11-18 19:39:39 +0100

    first try of implementing speaker dependent psi

--- matlab/mainczjs/em_algorithm.m
+++ matlab/mainczjs/em_algorithm.m
@@ -12,51 +12,87 @@ if nargin>2, em.iterations = iterations; fprintf('WARNING: Overriding EM-Iterati
 if nargin>3, em.conv_threshold = conv_threshold; fprintf('WARNING: Overriding EM convergence threshold (%d)!\n', conv_threshold); end
 if nargin<5, return_all_psi = false; end
 
-freq_mat = reshape(fft_freq_range,em.K,1,1,1,1);
-phi_mat = reshape(phi,em.K,em.T,1,1,room.R_pairs);
-phi_mat = repmat(phi_mat,1,1,em.Y-2*room.N_margin,em.X-2*room.N_margin,1);
+if ~(exist('ang_dist.mat', 'file') == 2)
+if ~(exist('ang_dist.mat', 'file') == 2)
+    % FREQ_MAT
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "fft_freq_range"); display(size(fft_freq_range)); end
+    freq_mat = reshape(fft_freq_range,em.K,1,1,1,1);
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "new freq mat"); display(size(freq_mat)); end
 
-m = 'Compute distances...'; counter = next_step(m, counter);
-norm_differences = zeros(em.Y-2*room.N_margin,em.X-2*room.N_margin,room.R_pairs);
-for idx_pairs = 1:room.R_pairs
-    for idx_x = (room.N_margin+1):(em.X-room.N_margin)
-        for idx_y = (room.N_margin+1):(em.Y-room.N_margin)
-            norm_differences(idx_y-room.N_margin,idx_x-room.N_margin,idx_pairs) = norm([room.grid_x(idx_x),room.grid_y(idx_y)]-room.R(idx_pairs*2,1:2),2) - norm([room.grid_x(idx_x),room.grid_y(idx_y)]-room.R(idx_pairs*2-1,1:2),2);
+    % PHI_MAT
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "phi"); display(size(phi)); end
+    %% extend phi_mat to sixth dimension for S and reshape into form!
+    % phi_mat = phi;
+    % for i=1:em.S-1
+    %     phi_mat = cat(4,phi_mat, phi);
+    % end
+    % phi_mat = reshape(phi_mat,em.K,em.T,1,1,em.M, em.S);
+    % if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "phi mat"); display(size(phi_mat)); end
+    phi_mat = reshape(phi,em.K,em.T,1,1,em.M);
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "phi mat"); display(size(phi_mat)); end
+    phi_mat = repmat(phi_mat,1,1,em.Ynet,em.Xnet,1, 1);
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "repmat phi mat"); display(size(phi_mat)); end
+
+    m = 'Compute distances...'; counter = next_step(m, counter);
+    norm_differences = zeros(em.Ynet,em.Xnet,em.M);
+    for idx_pairs = 1:room.R_pairs
+        for idx_x = (room.N_margin+1):(em.X-room.N_margin)
+            for idx_y = (room.N_margin+1):(em.Y-room.N_margin)
+                norm_differences(idx_y-room.N_margin,idx_x-room.N_margin,idx_pairs) = norm([room.grid_x(idx_x),room.grid_y(idx_y)]-room.R(idx_pairs*2,1:2),2) - norm([room.grid_x(idx_x),room.grid_y(idx_y)]-room.R(idx_pairs*2-1,1:2),2);
+            end
         end
     end
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "norm_diff"); display(size(norm_differences)); end
+    norm_differences = reshape(norm_differences,1,1,size(norm_differences,1),size(norm_differences,2),size(norm_differences,3));
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "new norm_diff"); display(size(norm_differences)); end
+    fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
+
+    %% Phi Tilde
+    m = 'Compute phi tilde...'; counter = next_step(m, counter);
+    phi_tilde_mat = exp(-1i*(bsxfun(@times,2*pi*freq(fft_freq_range), (norm_differences)/(room.c)))); % K/T/Y/X
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "phi tilde mat"); display(size(phi_tilde_mat)); end
+    % TODO: Make algorithm dependent on number of sources, evaluate
+    % performance (better, worse?)  --> additional dimension for phi and psi
+    clear norm_differences;
+    fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
+
+    %% Angular Distances
+    m = 'Compute angular distances...'; counter = next_step(m, counter);
+    phi_diff = bsxfun(@minus,phi_mat,phi_tilde_mat);
+
+    ang_dist = bsxfun(@power,abs(phi_diff),2);  % slower by about 1 sec on MBP
+    %   ang_dist = abs(phi_mat-phi_tilde_mat).^2;     % slightly slower (maybe 0.1 sec on MBP)
+    %   ang_dist = bsxfun(@power,abs(phi_mat-phi_tilde_mat),2);
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "ext. ang_dist"); display(size(ang_dist)); end
+
+    %% extend ang_dist to sixth dimension for S
+    ang_dist_temp = ang_dist;
+%     ang_dist = reshape(ang_dist, 1,)
+    for i=1:em.S-1
+        ang_dist = cat(6,ang_dist, ang_dist_temp);
+    end
+    ang_dist = permute(ang_dist, [6,1,2,3,4,5]);
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "ext. ang_dist"); display(size(ang_dist)); end
+
+    fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
+    clear phi_mat; clear phi_tilde_mat; clear phi_diff; clear ang_dist_temp;
+    save('ang_dist.mat', 'ang_dist', '-v7.3', '-nocompression');
+else
+    m = 'Load ang_dist from disk...'; counter = next_step(m, counter);
+    load('ang_dist.mat')
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "ang_dist"); display(size(ang_dist)); end
+    fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
 end
-
-norm_differences = reshape(norm_differences,1,1,size(norm_differences,1),size(norm_differences,2),size(norm_differences,3));
-fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
-
-%% Phi Tilde
-m = 'Compute phi tilde...'; counter = next_step(m, counter);
-phi_tilde_mat = exp(-1i*(bsxfun(@times,2*pi*freq(fft_freq_range), (norm_differences)/(room.c)))); % K/T/Y/X
-% TODO: Make algorithm dependent on number of sources, evaluate
-% performance (better, worse?)  --> additional dimension for phi and psi
-clear norm_differences;
-fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
-
-%% Angular Distances
-m = 'Compute angular distances...'; counter = next_step(m, counter);
-phi_diff = bsxfun(@minus,phi_mat,phi_tilde_mat);
-ang_dist = bsxfun(@power,abs((phi_diff)),2);  % slower by about 1 sec on MBP
-%   ang_dist = abs(phi_mat-phi_tilde_mat).^2;     % slightly slower (maybe 0.1 sec on MBP)
-%   ang_dist = bsxfun(@power,abs(phi_mat-phi_tilde_mat),2);
-fprintf('%s done! (t = %2.4f)\n', FORMAT_PREFIX, toc);
-clear phi_mat; clear phi_tilde_mat; clear phi_diff;
 
 %% EM Algorithm
 m = 'EM-Iterations...'; counter = next_step(m, counter);
 % Assign equal prior probabilities to each cluster.
-% TODO: Initialisierung anders gestalten? Eventuell besseres Ergebnis?
-psi = ones(em.Y-2*room.N_margin,em.X-2*room.N_margin,1) * (1 /(em.X-2*room.N_margin)*(em.Y-2*room.N_margin));
-% N_margin: Berechnungen nur für Bereich innerhalb der Mikrofone. TODO:
-% Ausweiten auf gesamten Bereich, auswerten ob Sources auch außerhalb erkannt werden.
+% IDEA: Initialisierung anders gestalten? Eventuell besseres Ergebnis?
+psi = ones(em.S, em.Ynet,em.Xnet,1) * (1 /(em.Xnet)*(em.Ynet));
+if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "psi"); display(size(psi)); end
+% TODO: Ausweiten auf gesamten Bereich (em.X instead of em.Xnet), auswerten ob Sources auch außerhalb erkannt werden.
 psi_old = zeros(size(psi));
 
-% variance = 0.1;%10; % Use the overal variance of the dataset as the initial variance for each cluster.
-% 09.11.2017: implemented parameter to provide fixed variance
 variance = em.var;
 var_ret = zeros(em.iterations+1, 1);
 var_ret(1) = em.var;
@@ -72,14 +108,19 @@ for iter = 1:em.iterations
     psi_old = psi;
 
 %% Expectation
-    pdf = bsxfun(@times,reshape(psi,1,1,em.Y-2*room.N_margin,em.X-2*room.N_margin,1),prod((1 / (variance * pi))*exp(-ang_dist / (variance)),5));
-
-    mu = bsxfun(@rdivide,pdf,reshape(sum(sum(pdf,4),3),em.K,em.T,1,1));
+%                                                                                 
+    prob = bsxfun(@times, reshape(psi,em.S,1,1,em.Ynet,em.Xnet,1), prod( (1/(variance*pi) ) *exp(-ang_dist/(variance)),6));
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "prob"); display(size(prob)); end
+    
+    mu = bsxfun(@rdivide,prob,reshape(sum(sum(prob,5),4),em.S,em.K,em.T));
     mu(isnan(mu)) = 0;
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "mu"); display(size(mu)); end
 
 %% Maximization
-    psi = squeeze(sum(sum(mu,2),1)/(em.T*em.K));
+    psi = squeeze(sum(sum(mu,3),2)/(em.T*em.K));
     psi(psi<=0) = eps;  % reset negative values to the smallest possible positive value
+    if verbose, fprintf("%s %15s: ", FORMAT_PREFIX, "maxim. psi"); display(size(psi)); end
+    
     if ~em.var_fixed  %Try to use fixed variance to improve results?
         var_denominator = squeeze(sum(sum(sum(sum(sum(bsxfun(@times,reshape(mu,size(mu,1),size(mu,2),size(mu,3),size(mu,4),1),ang_dist),5),4),3),2),1));
         var_numerator = room.R_pairs*squeeze(sum(sum(sum(sum(mu,4),3),2),1));
@@ -96,18 +137,18 @@ for iter = 1:em.iterations
     end
 end
 
-%% Delete outer margin (around microphones) to eliminate false peaks
-if size(psi_ret, 3) == 1  % return_all_psi == false
-    psi_ret(1,:) = 0;
-    psi_ret(size(psi, 1),:) = 0;
-    psi_ret(:,1) = 0;
-    psi_ret(:,size(psi, 2)) = 0;
-else  % return_all_psi == true
-    psi_ret(:, 1,:) = 0;
-    psi_ret(:, size(psi, 1),:) = 0;
-    psi_ret(:, :, 1) = 0;
-    psi_ret(:, :,size(psi, 2)) = 0;
-end
-psi_ret(iter+1:em.iterations,:,:) = [];  % remove zero rows, so size(psi, 1) gives actual em-iterations
-var_ret(iter+1:em.iterations) = [];  % remove zero rows, so size(psi, 1) gives actual em-iterations
+% %% Delete outer margin (around microphones) to eliminate false peaks
+% if size(psi_ret, 3) == 1  % return_all_psi == false
+%     psi_ret(1,:) = 0;
+%     psi_ret(size(psi, 1),:) = 0;
+%     psi_ret(:,1) = 0;
+%     psi_ret(:,size(psi, 2)) = 0;
+% else  % return_all_psi == true
+%     psi_ret(:, 1,:) = 0;
+%     psi_ret(:, size(psi, 1),:) = 0;
+%     psi_ret(:, :, 1) = 0;
+%     psi_ret(:, :,size(psi, 2)) = 0;
+% end
+% psi_ret(iter+1:em.iterations,:,:) = [];  % remove zero rows, so size(psi, 1) gives actual em-iterations
+% var_ret(iter+1:em.iterations) = [];  % remove zero rows, so size(psi, 1) gives actual em-iterations
 end
--- matlab/mainczjs/testrun.m
+++ matlab/mainczjs/testrun.m
@@ -1,4 +1,4 @@
-fprintf('------------------------- T E S T R U N -------------------------\n');
+fprintf('------------------------- T E S T R U N -------------------------\n'); tic;
 %% setting parameters
 sources = 5;
 md = 5;
@@ -6,24 +6,42 @@ wd = 12;
 rand_samples = true;
 T60=0.6;
 SNR=0;
-em_iterations=10;
+em_iterations=3;
 em_conv_threshold=0.0001;
 guess_randomly=false;
 reflect_order=3;
-var_init=10;
-var_fixed = false;
+var_init=1;
+var_fixed = true;
 get_em_history = false;
 
-%% init
-tic;
-fn_conf = config_update(sources, true, md,wd,rand_samples,T60,em_iterations, em_conv_threshold, reflect_order, SNR, var_init, var_fixed);
-load(fn_conf);
+w = what(); w = w.mat;
+saved_data = false;
+for i=1:length(w)
+    fname = cell2mat(w(i));
+    if sum(fname(1:6)=='config')==6
+        fn_cfg = fname;
+        load(fn_cfg);
+        saved_data = true;
+    end
+end
+if saved_data
+    fprintf("Will analyze saved data\n");
+else
+    fprintf("No saved data found! Will generate new!\n")
+end
+%% Create Simulation Environment
+if ~saved_data
+    %% init
+    tic;
+    fn_conf = config_update(sources, true, md,wd,rand_samples,T60,em_iterations, em_conv_threshold, reflect_order, SNR, var_init, var_fixed);
+    load(fn_conf);
 
-%% Simulate Environment
-x = simulate(fn_cfg, ROOM, R, sources);
+    %% Simulate Environment
+    x = simulate(fn_cfg, ROOM, R, sources);
 
-%% Calculate STFT
-[X, phi] = stft(fn_cfg, x);
+    %% Calculate STFT
+    [X, phi] = stft(fn_cfg, x);
+end
 
 %% Estimate Location (GMM+EM-Algorithmus)
 [psi, iterations, variance] = em_algorithm(fn_cfg, phi, em_iterations, em_conv_threshold, get_em_history);
@@ -46,6 +64,7 @@ fig = plot_results(psi_plot, loc_est, room);
 % saveas(fig, 'fig.fig', 'mfig');
 % matlab2tikz(strcat(PATH_SRC, '/latex/data/plots/static/', fname_trial, 'fig.tex'), 'figurehandle', fig, 'imagesAsPng', true, 'checkForUpdates', false, 'externalData', false, 'relativeDataPath', 'data/plots/static/tikz-data/', 'dataPath', PATH_LATEX_ABS, 'noSize', false, 'showInfo', false);
 close(fig);
+save(fn_cfg);  % save all temp results to config
 
 %% End
 fprintf('\n---------------------   E N D   ---------------------\n');
