a7abd36fddc5ac1acc41a272e71499b12c0a7e80
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-11-22 23:42:56 +0100

    need one more evalrun for 6 sources

--- matlab/mainczjs/evaluation/evalrun_lnt.m
+++ matlab/mainczjs/evaluation/evalrun_lnt.m
@@ -18,9 +18,10 @@ reflect_order=3;
 trials=25;
 var_init = 0.1;
 var_fixed = false;
-prior=[string('equal'), string('rand'), string('hh'), string('quart')];
+prior=[string('equal'), string('rand'), string('hh'), string('quart')]; %#ok<STRQUOT> string creation with "..." is not allowed on LNT Matlab.
 for p=1:length(prior)
-    for sources = 2:7
+    if strcmp(prior(p), string('equal')), minS = 6; else, minS = 2; end
+    for sources = minS:7
         random_sources_eval(description,sources,trials,md,wd,rand_samples,T60,SNR,em_iterations, em_conv_threshold, guess_randomly,reflect_order,var_init,var_fixed,dir,prior(p));
     end
 end
