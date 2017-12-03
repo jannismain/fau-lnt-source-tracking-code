3970d2fb7b5976995a78da7d5a5fabd2b0c8c829
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-12-03 20:39:30 +0100

    moved tracking plotting stuff into one folder

--- matlab/mainczjs_tracking/plotting/plot_variance.m
+++ matlab/mainczjs_tracking/plotting/plot_variance.m
@@ -1,4 +1,4 @@
-function plot_variance(variance, legend_labels, c, archive)
+function plot_variance(variance, legend_labels, c, archive, save_to)
 %  PLOT_VARIANCE Plots variance with form A x E x T, where A is an index for the algorithm
 %  used ('CREM', 'TREM'), E is an index for the Evaluation (var=0.1, var=0.5, ...) and T
 %  are the actual variance values.
@@ -8,16 +8,18 @@ function plot_variance(variance, legend_labels, c, archive)
 %      variance(2, E, :) = rem_tracking(...,'trem');
 %      plot_variance(variance, {'CREM', 'TREM'});
     
+% Parse input arguments, set default values if missing
     if nargin<2, legend_labels = {'CREM', 'TREM'}; end
     if nargin<3
         c.lmsred = [0.8000,0.2078,0.2196];
         c.darkgrey = [0.1, 0.1, 0.1];
     end
     if nargin<4, archive=false; end
+    if nargin<5, save_to=[getuserdir filesep 'latex' filesep 'plots' filesep 'tracking' filesep 'variance' filesep]; [~,~]=mkdir(save_to);end
+    %else, if ~strcmp(save_to(end), filesep), save_to = strcat(save_to, filesep); end; end
     
     E = size(variance, 2);
     T = size(variance, 3);
-    
     xvalues = linspace(1,T,T);
     fig = figure('Name','Variance'); hold on; box on; grid on;
     for e=1:E  % plotting the different evaluations in a loop allows us to create a correct legend
@@ -30,7 +32,7 @@ function plot_variance(variance, legend_labels, c, archive)
     xlim([0,T])
     legend(legend_labels{:},'Location','NorthEast')
     if archive
-        matlab2tikz(strcat(getuserdir, filesep, 'thesis', filesep, '/latex/data/plots/tracking/variance-comparison.tex'),...
+        matlab2tikz(char(strcat(save_to, 'variance-comparison.tikz')),...
                     'figurehandle', fig,...
                     'imagesAsPng', true,...
                     'checkForUpdates', false,...
