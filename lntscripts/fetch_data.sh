3fcd87c576a6ad6caeea2c24a946581da8550340
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-12-10 23:15:50 +0100

    fetch from results directory

--- lntscripts/fetch_data.sh
+++ lntscripts/fetch_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # fetch data from lnt server
-sshpass -f 'lntpw' rsync -avz --remove-source-files mainczyk@lnt94.e-technik.uni-erlangen.de:'/HOMES/mainczyk/thesis/src/matlab/mainczjs/evaluation/results/' '/Users/jannismainczyk/thesis/src/matlab/mainczjs/evaluation/results/'
+sshpass -f 'lntpw' rsync -avz --remove-source-files --exclude=config*.mat --exclude=raw/** mainczyk@lnt94.e-technik.uni-erlangen.de:'/HOMES/mainczyk/thesis/src/src/matlab/mainczjs/evaluation/results/' '/Users/jannismainczyk/thesis/src/matlab/mainczjs/evaluation/results/'
 
 #--exclude=config*.mat --exclude=raw/**
