d0bb246779e69755dd1443c430051c1f1e2dbde4
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2017-12-10 01:55:58 +0100

    T60 evalrun with sufficient rir.length

--- matlab/mainczjs/evaluation/evalrun_lnt.m
+++ matlab/mainczjs/evaluation/evalrun_lnt.m
@@ -4,21 +4,19 @@ addpath_recurse  % ensure all folders are added to path
 rand('state',sum(100*clock));
 
 % EVALUATION ROUTINE:
-description='min-distance';
-trials=[25 50 25];
-md = [3 5 10];
+description='T60';
+trials=50;
+md = 5;
 wd = 12;
 rand_samples = true;
-T60=0.3;
+T60=0.9;
 SNR=0;
 em_iterations=5;
 em_conv_threshold=-1;
 guess_randomly=false;
-reflect_order=3;
+reflect_order=-1;
 var_init = 0.1;
 var_fixed = false;
 for sources = 2:7
-    for i=1:length(md)
-        random_sources_eval(description,sources,trials(i),md(i),wd,rand_samples,T60,SNR,em_iterations,em_conv_threshold,guess_randomly,reflect_order,var_init,var_fixed,'/HOMES/mainczyk/thesis/src/');
-    end
+        random_sources_eval(description,sources,trials,md,wd,rand_samples,T60,SNR,em_iterations,em_conv_threshold,guess_randomly,reflect_order,var_init,var_fixed,'/HOMES/mainczyk/thesis/src/');
 end
